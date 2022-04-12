ea5485afff2c9d2ebc81ed7b8f5e359ffa04d8e5
colbertgendreau <97406341+colbertgendreau@users.noreply.github.com>
2022-04-12 16:05:39 -0400

    changed some stuff

---
+++ script.js
@@ -0,0 +1,14 @@
+document.querySelector('#button').addEventListener('click', () => {
+    console.log('Message // versionner ici');
+  });
+
+
+  document.querySelector('#button').addEventListener('click', () => {
+    console.warn('Attention! //Versionner encore');
+});
+
+
+
+  document.querySelector('#button').addEventListener('click', () => {
+    console.error(`ERREUR //Versionner une autre fois.`);
+  });   
